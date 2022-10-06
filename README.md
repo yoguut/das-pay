b4986db95fbf7df68cd2b8eae0748001757dfb40
user <user@host.com>
2022-10-06 00:10:07 -0700

    tmp save

--- README.md
+++ README.md
@@ -4,13 +4,15 @@ People's payment engine CLI.
 
 ## Get started
 
-Clone the repository:
+First, make sure to use a rustc version higher than `1.31` since it's required by `serde`'s `derive` feature.
+
+Next, clone the repository:
 
 ```bash,ignore
 git clone git@github.com:yoguut/das-pay.git
 ```
 
-Since this repo tracks large files as pointers to a lfs bucket, lfs needs to be pulled separately as followed:
+Next, since this repo tracks large files as pointers to a lfs bucket, lfs needs to be pulled separately as followed:
 
 ```bash,ignore
 git lfs pull
--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,23 @@ fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::new(file_path).context("Cannot find input file")?;
     let mut rdr = csv::Reader::from_path(opt.path)?;
     let mut results = rdr.deserialize();
+    let mut summary: Vec<Transaction> = vec![];
+
     for result in results {
+        // coercing `DeserializeRecordsIter` hurts performance
         let trans: Transaction = result?;
+        summary.push(trans);
+        // eprintln!("{:?}", trans);
+    }
+
+    let mut wtr = csv::Writer::from_writer(summary);
+    wtr.serialize
+    Ok(())
+}
+
+fn audit_tansactions(transactions: &mut DeserializeRecordsIter) -> Result<(), Box<dyn Error>> {
+    for x in transactions {
+        let trans: Transaction = x?;
         eprintln!("{:?}", trans);
     }
     Ok(())
